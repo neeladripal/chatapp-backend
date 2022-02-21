f7ad5c00470357d447449b5875a09eb2b0168798
Neeladri Pal <neel.pal26@gmail.com>
2022-02-21 21:20:44 +0530

    Specify cors origin

--- startup/config.js
+++ startup/config.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const cors = require("cors");
 
 module.exports = function (app) {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URI,
+    })
+  );
   // in case the jwtPrivateKey environment variable is not defined
   if (!process.env.jwtPrivateKey) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
