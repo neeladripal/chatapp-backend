18ce88e1f9dfa7d81f3364c074a98dd8a695ba8c
Neeladri Pal <neel.pal26@gmail.com>
2022-02-21 16:07:00 +0530

    Bare backend

---
+++ startup/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const cors = require("cors");
+
+module.exports = function (app) {
+  app.use(cors());
+  // in case the jwtPrivateKey environment variable is not defined
+  if (!process.env.jwtPrivateKey) {
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
+  }
+};
